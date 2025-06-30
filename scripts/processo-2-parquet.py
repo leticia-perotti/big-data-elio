2c8bd49fdf013468ae929795673f6628b9b066ca
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2025-06-29 21:38:15 -0300

    scripts

---
+++ scripts/processo-2-parquet.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+import time
+
+spark = SparkSession.builder \
+    .appName("Process CSV por Cliente") \
+    .master("spark://spark-master:7077") \
+    .getOrCreate()
+
+start = time.time()
+
+df = spark.read.option("header", "true").csv("hdfs://namenode:8020/user/spark/bases/dados_sinteticos_1000000.parquet").repartition(8)
+
+df.groupBy("cliente_id").count().show()
+
+end = time.time()
+print(f"Tempo de processamento CSV: {end - start:.2f} segundos")
+
+spark.stop()
